fe54a10909289d51eac3890ffa812db53275d861
FatimaBangayadiallo <fatimabangaya95@gmail.com>
2024-12-24 16:46:24 +0000

    the spices project

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// ********** set date ************
+// select span
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+// navBtn.addEventListener("click", () => {
+//   links.classList.toggle("show-links");
+// });
+
+navBtn.addEventListener('click', function(){
+  const result = links.classList.contains('show-links');
+  
+  if(result){
+    links.classList.remove('show-links');
+  }else{
+    links.classList.add('show-links');
+  }
+})
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
